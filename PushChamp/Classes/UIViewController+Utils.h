d9257a2d8cde6244d2d4f683ea7e7220cce25fa2
Nithin M Das <nithin@voltella.com>
2017-01-09 15:23:28 +0530

    Added events management

---
+++ PushChamp/Classes/UIViewController+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Utils.h
+//  Test Analytics SDK
+//
+//  Created by Nithin M Das on 02/01/17.
+//  Copyright © 2017 NMD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Utils)
+
++(UIViewController*) currentViewController;
+
+@end
